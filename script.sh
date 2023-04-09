1902ef2327982c51592e78bfe0298a5225ac4f25
deivid123-skcet <deividkelven1@hotmail.com>
2023-04-08 21:16:37 -0300

    primeiro commit

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##Adicionando Repositorio do Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+## Instalar Terraform, Ansible e Python3
+sudo apt update -y && sudo apt install terraform python3  -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible && sudo apt-get install ansible -y
+
+##Instala AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
